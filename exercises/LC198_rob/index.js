008d3d576e396af3acd54cd2f35186a676c4eaeb
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-11-12 14:58:37 -0800

    LC53, 198

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -1,3 +1,22 @@
-function rob(nums) {}
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+
+function rob(nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[ 0 ];
+    if (nums.length === 2) return Math.max(nums[ 0 ], nums[ 1 ]);
+
+    let maxLootAtNth = [ nums[ 0 ], Math.max(nums[ 0 ], nums[ 1 ]) ];
+
+    for (let i = 2; i < nums.length; i++) {
+        maxLootAtNth.push(
+            Math.max(nums[ i ] + maxLootAtNth[ i - 2 ], maxLootAtNth[ i - 1 ])
+        );
+    }
+
+    return maxLootAtNth.pop();
+}
 
 module.exports = rob;
--- exercises/LC53_maxSubArray/index.js
+++ exercises/LC53_maxSubArray/index.js
@@ -1,3 +1,17 @@
-function maxSubArray(nums) {}
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+function maxSubArray(nums) {
+    //let dp = [ nums[ 0 ] ]; O(n)
+    let max = nums[ 0 ];
+
+    for (let i = 1; i < nums.length; i++) {
+        const num = nums[ i ];
+        nums[ i ] = Math.max(num, num + nums[ i - 1 ]);
+        max = Math.max(max, nums[ i ]);
+    }
+    return max;
+}
 
 module.exports = maxSubArray;
