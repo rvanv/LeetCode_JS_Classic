c2dfd26b69cd1b67765bab1b65d363b03c3ef34a
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-11-12 11:03:20 -0800

    lc217, lc238

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,15 @@
-const containsDuplicate = nums => {};
+const containsDuplicate = nums => {
+    const vistedNums = {}; // {7: true, 2: true} 
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[ i ];
+        if (vistedNums[num]) {
+            return true;
+        } else {
+            vistedNums[ num ] = true;
+        }
+    }
+    return false;
+};
 
 module.exports = containsDuplicate;
--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,25 @@
-const productExceptSelf = nums => {};
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+const productExceptSelf = nums => {
+    const output = nums.map(n => 1);
+    let product = 1;
+
+    //multiplyLeft
+    for (let i = 0; i < nums.length; i++) {
+        output[ i ] = output[i] * product;
+        product = product * nums[ i ];
+    };
+
+    product = 1;
+    //multiplyRight
+    for (let j = nums.length - 1; j >= 0; j--) {
+
+        output[ j ] = output[j] * product;
+        product = product * nums[ j ];
+    };
+
+    return output;
+};
 
 module.exports = productExceptSelf;
